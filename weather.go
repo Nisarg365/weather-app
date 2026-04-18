f39f3bf0b461977a79c694750fb88e06eef87e74
Nisarg365 <179633510+Nisarg365@users.noreply.github.com>
2026-04-18 10:27:38 +0100

    Updated loop logic

--- weather.go
+++ weather.go
@@ -6,18 +6,32 @@ import (
 
 func main() {
 	var temp int
+	for {
+		fmt.Println("--- Smart Weather Bot ---")
+		fmt.Print("What is the temperature in Celsius? ")
 
-	fmt.Println("--- Smart Weather Bot ---")
-	fmt.Print("What is the temperature in Celsius? ")
-	fmt.Scan(&temp)
-	if temp > 25 {
-		fmt.Println("Wear a T-shirt ☀️")
-	} else if temp < 15 {
-		fmt.Println(" Bring a Heavy coat ❄️")
-	} else if temp >= 15 && temp <= 25 {
-		fmt.Println("Bring a jacket 🧥")
-	} else if temp == 21 {
-		fmt.Println("21°C? That is the perfect temperature for a PhD student! 🎓")
+		_, err := fmt.Scan(&temp)
+
+		if err != nil {
+			fmt.Println("❌ Error: Please enter a whole number!")
+
+			var discard string
+			fmt.Scanln(&discard)
+
+			continue
+		}
+
+		if temp == 999 {
+			fmt.Println("Exit")
+			break
+		} else if temp == 21 {
+			fmt.Println("21°C? That is the perfect temperature for a PhD student! 🎓")
+		} else if temp > 25 && temp != 999 {
+			fmt.Println("Wear a T-shirt ☀️")
+		} else if temp < 15 {
+			fmt.Println("Bring a Heavy coat ❄️")
+		} else {
+			fmt.Println("Bring a jacket 1🧥")
+		}
 	}
 }
-
